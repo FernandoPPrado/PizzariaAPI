6060d4161db93eb7e66bf2adb7b0b462b1423c65
FernandoPPrado <fernandopradoifc@gmail.com>
2025-10-28 21:00:48 -0300

    main - Implement product entity, repository and category enum

---
+++ src/main/java/com/pizzaria/demo/product/model/Product.java
@@ -0,0 +1,76 @@
+package com.pizzaria.demo.product.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String productName;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private boolean active;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+
+    public Product() {
+    }
+
+    public Product(String productName, String description, BigDecimal price, boolean active) {
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+        this.active = active;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/pizzaria/demo/product/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.pizzaria.demo.product.repository;
+
+import com.pizzaria.demo.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findByProductName(String productName);
+}
