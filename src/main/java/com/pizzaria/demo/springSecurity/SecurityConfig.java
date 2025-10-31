0c99ffe374e4a245f93e8e40a3a9a6c20ec7e87a
FernandoPPrado <fernandopradoifc@gmail.com>
2025-10-30 21:03:10 -0300

    main - Refactor JWT authentication and exception handling for improved security

--- src/main/java/com/pizzaria/demo/springSecurity/SecurityConfig.java
+++ src/main/java/com/pizzaria/demo/springSecurity/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -17,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity // habilita @PreAuthorize, @PostAuthorize, etc.
 @RequiredArgsConstructor
 public class SecurityConfig {
 
