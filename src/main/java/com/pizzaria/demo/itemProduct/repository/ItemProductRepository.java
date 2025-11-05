67735e948db08b06816630705189b7cdcf4f388d
FernandoPPrado <fernandopradoifc@gmail.com>
2025-11-05 20:05:16 -0300

    main - Implement ItemProductRepository

---
+++ src/main/java/com/pizzaria/demo/itemProduct/repository/ItemProductRepository.java
@@ -0,0 +1,14 @@
+package com.pizzaria.demo.itemProduct.repository;
+
+import com.pizzaria.demo.itemProduct.model.ItemProduct;
+import com.pizzaria.demo.purchase.model.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemProductRepository extends JpaRepository<ItemProduct, Integer> {
+
+    List<ItemProduct> findAllByPurchaseIdAndEnabledTrue(Integer purchaseId);
+
+
+}
