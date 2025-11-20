6d38f8c1144dec97adb66d90fb0fa66876bd1ae6
FernandoPPrado <fernandopradoifc@gmail.com>
2025-11-19 21:22:41 -0300

    main - Add webhook secret validation to Mercado Pago controller

--- src/main/java/com/pizzaria/demo/mercadoPago/controller/MercadoPagoWebhookController.java
+++ src/main/java/com/pizzaria/demo/mercadoPago/controller/MercadoPagoWebhookController.java
@@ -2,22 +2,36 @@ package com.pizzaria.demo.mercadoPago.controller;
 
 import com.pizzaria.demo.mercadoPago.dto.MercadoPagoWebhookDTO;
 import com.pizzaria.demo.mercadoPago.service.MercadoPagoService;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/mercado-pago")
 public class MercadoPagoWebhookController {
 
     private final MercadoPagoService mercadoPagoService;
 
-    public MercadoPagoWebhookController(MercadoPagoService mercadoPagoService) {
+    private final String webhookSecret;
+
+    public MercadoPagoWebhookController(MercadoPagoService mercadoPagoService, @Value("${webhook.secret}") String webhookSecret) {
         this.mercadoPagoService = mercadoPagoService;
+        this.webhookSecret = webhookSecret;
+
     }
 
     @PostMapping("/webhook")
-    public ResponseEntity<Void> webhook(@RequestBody MercadoPagoWebhookDTO webhookDTO) {
-        System.out.println(">>> Recebi webhook do MP: " + webhookDTO.data().id());
+    public ResponseEntity<Void> webhook(@RequestParam(name = "secret", required = false) String secret, @RequestBody MercadoPagoWebhookDTO webhookDTO) {
+        log.info("Webhook do MercadoPago, iniciando validacao");
+
+        if (secret == null || secret.isBlank() || !secret.equals(webhookSecret)) {
+            log.warn("Webhook do Mercado Pago com secret inválido ou ausente");
+            return ResponseEntity.status(403).build();
+        }
+        log.info("Webhook do Mercado Pago recebido com secret válido");
         mercadoPagoService.processWebhook(webhookDTO);
         return ResponseEntity.ok().build();
     }
