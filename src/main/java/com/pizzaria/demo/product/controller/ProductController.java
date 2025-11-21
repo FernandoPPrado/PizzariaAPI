345c0a531be3fd7c53669675fc68b4ca0cbc1a40
FernandoPPrado <fernandopradoifc@gmail.com>
2025-11-20 21:29:24 -0300

    main - Refactor products path to product in ProductController

--- src/main/java/com/pizzaria/demo/product/controller/ProductController.java
+++ src/main/java/com/pizzaria/demo/product/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/products")
+@RequestMapping(path = "/product")
 public class ProductController {
 
     private final ProductService productService;
